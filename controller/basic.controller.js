1965699dd9770c3800360e9c3a8e94c06a643782
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2023-12-11 18:57:07 +0530

    add dashbord page

--- controller/basic.controller.js
+++ controller/basic.controller.js
@@ -2,18 +2,43 @@ const UserModel = require("../model/user.model");
 
 const BasicController = {
   homePage(request, response) {
-    response.render("dashboard");
+    if (request.session.login === undefined) {
+      response.redirect("/login");
+      return false;
+    }
+    response.render("dashboard",{
+      login:request.session.login,
+    });
   },
+  logout(request, response) {
+    delete request.session.login;
+    response.redirect("/login");
+  },
+
   loginPage(request, response) {
-    response.render("login");
+    if (request.session.login !== undefined) {
+      response.redirect("/");
+      return false;
+    }
+    let message =
+      request.session.message !== undefined ? request.session.message : "";
+    delete request.session.message;
+    response.render("login", {
+      message: message,
+    });
   },
 
   registrationPage(request, response) {
+    if (request.session.login !== undefined) {
+      response.redirect("/");
+      return false;
+    }
     let message =
       request.session.message !== undefined ? request.session.message : "";
     delete request.session.message;
     response.render("new_registration", {
       message: message,
+      newUser: { ...request.session.newUser },
     });
   },
 
@@ -27,21 +52,67 @@ const BasicController = {
         email: data.email,
       });
 
-      UserModel.find({email:{$regex:"^"+email+"$", Option:"i"}})
-
-
-      let result = await newUser.save();
-      if (result) {
-        request.session.message = "user Registration Done Successfully";
+      let isUserExists = await UserModel.findOne({
+        email: { $regex: "^" + data.email + "$", $options: "i" },
+      });
+      if (isUserExists) {
+        request.session.message = "Email already taken, try another one";
+        request.session.newUser = { ...data };
+        response.redirect("/new_registration");
       } else {
-        request.session.message = "Unable to save user, try again";
+        let result = await newUser.save();
+        if (result) {
+          request.session.message =
+            "User Registration Done Successfully, login now";
+          request.session.newUser = {};
+          response.redirect("/login");
+        } else {
+          request.session.message = "Unable to save user, try again";
+          request.session.newUser = { ...data };
+          response.redirect("/new_registration");
+        }
       }
-      response.redirect("/new_registration");
     } catch (error) {
       request.session.message = "Unable to save user, try again";
+      request.session.newUser = { ...data };
       response.redirect("/new_registration");
     }
   },
+
+  async removeUsers(request, response) {
+    let result = await UserModel.deleteMany({});
+    response.json({
+      status: true,
+      result,
+    });
+  },
+
+  async userLogin(request, response) {
+    let data = request.body;
+    try {
+      let user = await UserModel.findOne(
+        {
+          email: { $regex: `^${data.email}$`, $options: "i" },
+          password: data.password,
+        },
+        { password: 0 }
+      );
+      if (user) {
+        request.session.login = { user };
+        response.redirect("/");
+      } else {
+        request.session.message = "Username or password is wrong, try again";
+        response.redirect("/login");
+      }
+    } catch (error) {
+      request.session.message = "Something went wrong, try again";
+      response.redirect("/login");
+    }
+  },
+  async saveProduct(request, response){
+    let data = request.body;
+    response.json({status:true, data})
+  }
 };
 
 module.exports = BasicController;
--- routes/basic.routes.js
+++ routes/basic.routes.js
@@ -1,13 +1,21 @@
+const multer = require("multer");
+const upload = multer();//add multipart from data
 const BasicController = require("../controller/basic.controller");
 
 const BasicRouter = require("express").Router();
 
-BasicRouter.get('/',BasicController.homePage);
-BasicRouter.get('/login',BasicController.loginPage);
-BasicRouter.get('/new_registration',BasicController.registrationPage);
+BasicRouter.get("/", BasicController.homePage);
+BasicRouter.get("/login", BasicController.loginPage);
+BasicRouter.get("/new_registration", BasicController.registrationPage);
+BasicRouter.get("/remove-user", BasicController.removeUsers);
+BasicRouter.get("/logout", BasicController.logout);
 
-BasicRouter.post('/save-user',BasicController.saveUser)
-
+BasicRouter.post("/save-user", BasicController.saveUser);
+BasicRouter.post("/user-login", BasicController.userLogin);
+BasicRouter.post(
+  "/api/save-new-product",
+  upload.none(),
+  BasicController.saveProduct
+);
 
 module.exports = BasicRouter;
-
